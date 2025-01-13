202540c173cfd736ea403ffbf98eae9892bb888a
saga-k <sagakrusell+1@gmail.com>
2025-01-13 16:28:14 +0100

    Add and delete works

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const app = Vue.createApp({});
 
 app.component('list', {
+
+  created() {
+    this.getStored();
+  },
+
   data() {
     return {
       listItems: [
-        {
-          task: 'Initiate Github repo',
-          checked: true
-        },
-        {
-          task: 'Setup Vue',
-          checked: false
-        },
-        {
-          task: 'Create list component',
-          checked: false
-        }]
+
+      ]
     }
   },
 
   template: `
-<ul>
-<div v-for='listItem in listItems' :class = "{checked: listItem.checked}" class = "renderedList">
-  <input :checked = "listItem.checked" type = 'checkbox' value = listItem.task @click = checkItem(listItem)>
-  <li>{{listItem.task}}</li>
-  <div class=removeIcon @click = "removeItem(listItem)"></div>
-</div>
-</ul>
-`,
+  <ul>
+      <div v-for='listItem in listItems' :class = "{checked: listItem.checked}" class = "renderedList">
+        <input :checked = "listItem.checked" type = 'checkbox' value = listItem.task @click = checkItem(listItem)>
+        <li>{{listItem.task}}</li>
+        <div class=removeIcon @click = "removeItem(listItem)"></div>
+      </div>
+    <addNew @add-item="receiveEmit"></addNew>
+  </ul>
+  `,
 
   methods: {
     removeItem(listItem) {
@@ -36,7 +32,9 @@ app.component('list', {
         if (this.listItems[i] === listItem) {
           this.listItems.splice(i, 1)
         }
-      }
+      };
+
+      this.setStorage();
 
     },
 
@@ -45,10 +43,54 @@ app.component('list', {
         listItem.checked = false
       } else {
         listItem.checked = true
-      }
+      };
+
+      this.setStorage();
+    },
+
+    receiveEmit(newTask) {
+      console.log('emit recieved' + newTask);
+      this.listItems.push({
+        task: newTask,
+        checked: false
+      });
+      this.setStorage();
+    },
+
+    getStored() {
+      let fromStorage = JSON.parse(localStorage.getItem('stored'));
+      console.log(fromStorage);
+      this.listItems = fromStorage
+    },
+
+    setStorage() {
+      let stored = JSON.stringify(this.listItems);
+      localStorage.setItem('stored', stored);
+    }
+  }
+});
+
+
+app.component('addNew', {
+  data() {
+    return {
+      newTask: '',
+    }
+  },
+
+  emits: ['add-item'],
+
+  template: `
+  <input type="text" v-model = 'newTask'></input>
+  <button @click='onClick'>add</button>
+`,
+
+  methods: {
+    onClick() {
+      this.$emit('add-item', this.newTask);
+      this.newTask = ''
     }
   }
 })
-
 
 app.mount('#app');
